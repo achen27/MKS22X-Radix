1622bb12991424c14ddd8fbb4e73627e009e1e1d
Amanda Chen <achen01@stuy.edu>
2019-04-01 10:55:54 -0400

    found largest integer

--- Radix.java
+++ Radix.java
@@ -1,7 +1,11 @@
 public class Radix{
   public static void radixsort(int[]data){
-    int largest = 0;
-    
+    int largest = data[0];
+    for (int i = 1; i < data.length; i++){
+      if (data[i]>largest){
+        largest = data[i];
+      }
+    }
     int place = 1;
     MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
 
