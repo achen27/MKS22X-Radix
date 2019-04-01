3a6d81a7d145bd5f8d830afc2a4499b084cc2fb0
Amanda Chen <achen01@stuy.edu>
2019-04-01 10:54:43 -0400

    created buckets

---
+++ Radix.java
@@ -0,0 +1,9 @@
+public class Radix{
+  public static void radixsort(int[]data){
+    int largest = 0;
+    
+    int place = 1;
+    MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
+
+  }
+}
