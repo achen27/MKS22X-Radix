14dc388778fea2b304b388227d30429ab42053c1
Amanda Chen <achen01@stuy.edu>
2019-04-01 22:33:05 -0400

    merged linked list on first pass

--- Radix.java
+++ Radix.java
@@ -14,12 +14,16 @@ public class Radix{
 
     while (largest % place != 0){
       if (place == 1){
-        for (int i = 1; i < data.length; i++){
+        for (int i = 0; i < data.length; i++){
           if (data[i] >= 0){
             buckets[data[i]%10+10].add(data[i]);
           } else {
             buckets[9+data[i]%10].add(data[i]);
           }
+        }
+        sorting = buckets[0];
+        for (int i = 1; i < buckets.length; i++){
+          sorting.extend(buckets[i]);
         }
       } else {
 
