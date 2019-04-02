ead95b2db8cb278ea7efdec91a7989c287f3cfe0
Amanda Chen <achen01@stuy.edu>
2019-04-01 22:30:03 -0400

    wrote loop for first pass

--- Radix.java
+++ Radix.java
@@ -1,13 +1,31 @@
 public class Radix{
   public static void radixsort(int[]data){
+
     int largest = data[0];
     for (int i = 1; i < data.length; i++){
       if (data[i]>largest){
         largest = data[i];
       }
     }
+
     int place = 1;
     MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
+    MyLinkedList<Integer> sorting = new MyLinkedList();
+
+    while (largest % place != 0){
+      if (place == 1){
+        for (int i = 1; i < data.length; i++){
+          if (data[i] >= 0){
+            buckets[data[i]%10+10].add(data[i]);
+          } else {
+            buckets[9+data[i]%10].add(data[i]);
+          }
+        }
+      } else {
+
+      }
+      place *=10;
+    }
 
   }
 }
